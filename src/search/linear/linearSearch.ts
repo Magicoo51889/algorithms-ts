37b9e60aced3e137132dffd672193567a448abb2
Jack Foot <jackot11f@gmail.com>
2021-11-25 09:20:48 +0000

    Linear search

--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -1,6 +1,17 @@
 import { NO_MATCH } from "../common";
 
 const linearSearch = (items: any[], item: any): number => {
+    let found:boolean = false
+    let index:number = 0
+
+    for(let i=0; i<items.length; i++){
+        if (items[i] === item) {
+            let found:boolean = true
+            let index:number = i
+            console.log(found + " " + index)
+            return i;
+        }
+    }
 
     return NO_MATCH;
 }
