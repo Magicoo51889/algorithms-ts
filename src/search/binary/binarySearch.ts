543cb9e8868baaf6d14394c04e55c00818bc85bf
Jack Foot <jackot11f@gmail.com>
2021-11-25 09:27:02 +0000

    Update binarySearch.ts

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,6 +1,14 @@
 import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any): number => {
+    var lowestIndex:number = 0;
+    var highestIndex:number = items.length - 1;
+    var completed:boolean = false
+
+
+    while (lowestIndex <= highestIndex){
+        var middleIndex = Math.floor((highestIndex + lowestIndex)/2);
+    }
 
     return NO_MATCH;
 }
