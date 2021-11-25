4efa10ee0137b416710b5b0f4f410ee8f0b80cb0
Jack Foot <jackot11f@gmail.com>
2021-11-25 11:01:03 +0000

    Update binarySearch.ts

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,33 +1,26 @@
 import { NO_MATCH } from "../common"
 
-const binarySearch = (items: any[], item: any): number => {
-    var lowestIndex:number = 0;
-    var highestIndex:number = items.length - 1;
-    var completed:boolean = false
+const alphabet =["abcdefghijklmnopqrstuvwxyz"]
 
+const binarySearch = (items: any[], item: any) => {
+    let lowestIndex:number = 0;
+    let highestIndex:number = items.length - 1;
+    let completed:boolean = false
 
-    while (lowestIndex <= highestIndex){ // Infinite loop
-        var middleIndex = Math.floor((highestIndex + lowestIndex)/2);
-        if (items[middleIndex] == item) {
-            console.log("Lowest: " + lowestIndex)
-            console.log("Highest: " + highestIndex)
-            console.log("Mid: " + middleIndex)
-            completed = true
-            return middleIndex;
-        } else if (items[middleIndex] < item){
-            lowestIndex = middleIndex + 1;
-            console.log("Lowest: " + lowestIndex)
-            console.log("Highest: " + highestIndex)
-            console.log("Mid: " + middleIndex)
-        } else {
-            lowestIndex = middleIndex - 1;
-            console.log("Lowest: " + lowestIndex)
-            console.log("Highest: " + highestIndex)
-            console.log("Mid: " + middleIndex)
-        }
-    } return NO_MATCH;
-    
-    
+    if (typeof(item) === "number"){
+        while ((lowestIndex <= highestIndex) && completed){ // Infinite loop
+            let middleIndex = lowestIndex + Math.floor((highestIndex - lowestIndex)/2);
+            let guess = items[middleIndex];
+            if (guess === item) {
+                completed = true
+                return middleIndex;
+            } else if (items[middleIndex] < item){
+                lowestIndex = middleIndex + 1;
+            } else {
+                lowestIndex = middleIndex - 1;
+            }
+        } return NO_MATCH;
+    }
 }
 
 export default binarySearch;
