e6eaca4f31cf6b06c1c53d22e5564fdcf161d7d8
Jack Foot <jackot11f@gmail.com>
2021-12-07 12:34:49 +0000

    Update binarySearch.ts

--- src/search/binary/binarySearch.ts
+++ src/search/binary/binarySearch.ts
@@ -1,41 +1,17 @@
 import { NO_MATCH } from "../common"
 
 const binarySearch = (items: any[], item: any) => {
-    let result = null
     let lowestPointer:number = 0;
-    let highestPointer:number = items.length - 1;
+    let highestPointer:number = items.length;
 
-    if (typeof(item) === "number"){
-        while (lowestPointer <= highestPointer && result == null){
-            let midPointer = lowestPointer + Math.floor((highestPointer - lowestPointer)/2);
-            let result = items[midPointer];
-            if (items[midPointer] === item) {
-                result === items[midPointer]
-                console.log(result)
-                return result;
-            } else if (items[midPointer] < item){
-                if (midPointer === items.length){
-                    console.log(midPointer)
-                    console.log(result)
-                    return NO_MATCH
-                }
-                lowestPointer = midPointer + 1;
-            }else if (lowestPointer === highestPointer){
-                console.log(lowestPointer)
-                console.log(highestPointer)
-                return NO_MATCH
-            } else {
-                if (midPointer === 0){
-                    console.log(lowestPointer)
-                    console.log(highestPointer)
-                    console.log(midPointer)
-                    return NO_MATCH
-                }
-                highestPointer = midPointer - 1;
-            }
-        } return NO_MATCH
-    }
+    while (lowestPointer < highestPointer){
+        let midPointer = Math.floor((highestPointer + lowestPointer)/2);
+        if (items[midPointer] === item) return midPointer;
+        if (items[midPointer] < item) lowestPointer = midPointer + 1;
+        if (items[midPointer] > item) highestPointer = midPointer;
+        if (lowestPointer === highestPointer) return NO_MATCH;
+        if (midPointer === 0) return NO_MATCH;
+    } return NO_MATCH
 }
-// returns 7 not 5
-
 export default binarySearch;
+
