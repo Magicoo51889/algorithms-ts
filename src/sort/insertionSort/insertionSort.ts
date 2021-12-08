b7ae03dcbcb2435855c6b1845bd7c98f51bb1c6d
Jack Foot <jackot11f@gmail.com>
2021-12-08 10:27:26 +0000

    Update insertionSort.ts

--- src/sort/insertionSort/insertionSort.ts
+++ src/sort/insertionSort/insertionSort.ts
@@ -3,9 +3,25 @@ import swap from "../swap";
 
 const insertionSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
 
-    // Your code here
+    let output_list = items
+    for (let i = 0; i < items.length; i++) {
+        let item_to_place = output_list[i]
+        let item_place: number = i
 
-    return items;
+        while (item_place > 0){
+            let lower: number = item_place - 1;
+            let upper: number = item_place;
+
+            if (output_list[lower] > item_to_place){
+                swap(output_list, lower, upper)
+            } else {
+                item_place = upper
+                break
+            } item_place -= 1
+        }
+        output_list[item_place] = item_to_place
+    }
+    return output_list;
 }
 
 export default insertionSort;
