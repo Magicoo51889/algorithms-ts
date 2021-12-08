4aca755909cdda3052a9159903cc3819dfcc18fc
Jack Foot <jackot11f@gmail.com>
2021-12-08 21:56:59 +0000

    Update bubbleSort.ts

--- src/sort/bubbleSort/bubbleSort.ts
+++ src/sort/bubbleSort/bubbleSort.ts
@@ -2,10 +2,21 @@ import { SortAlgorithm } from "../common";
 import swap from "../swap";
 
 const bubbleSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
+    let output_list = items
 
-    // Your code here
+    for (let t = 0; t < items.length; t++) {
+        var swap_made: boolean = false
 
-    return items;
+        for (let c = t - 1; c++;) {
+            if (output_list[c] > output_list[c + 1]){
+                swap_made = true
+                swap(output_list, c, c + 1);
+            }
+        } if(swap_made === false){
+            break
+        } 
+    }
+    return output_list;
 }
 
 export default bubbleSort;
