0d9dd47efcbb529d7912352c8ab5b2baa7290bf6
Jack Foot <jackot11f@gmail.com>
2021-12-10 09:08:24 +0000

    Update mergeSort.ts

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -1,23 +1,24 @@
 import { SortAlgorithm } from "../common";
 
+type StrOrInt = string | number;
+
 const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
     if (items.length === 0){
         return items
     }
-    return merge_sort_recurse(input_list, 0, input_list.length)
+    return merge_sort_recurse(items, 0, items.length)
     
-    function merge_sort_recurse<T>(input_list: StrOrInt,
+    function merge_sort_recurse<T>(items: StrOrInt,
                                 left_pointer: number,
-                                right_pointer: number) -> items {
-    if (left_pointer == right_pointer){
-        return input_list[left_pointer]
+                                right_pointer: number) {
+    if (left_pointer === right_pointer){
+        return items[left_pointer]
     }
-    middle: number = floor((left_pointer + right_pointer) / 2)
+    let middle: number = floor((left_pointer + right_pointer) / 2)
+    let first_half: StrOrInt = merge_sort_recurse(items, left_pointer, middle);
+    let second_half: StrOrInt = merge_sort_recurse(items, middle + 1, right_pointer);
 
-    first_half: StrOrInt = merge_sort_recurse(input_list, left_pointer, middle);
-    second_half: StrOrInt = merge_sort_recurse(input_list, middle + 1, right_pointer);
-
-    output_list: StrOrInt = []
+    let output_list: List[StrOrInt] = []
     while(((first_half.length) > 0) && ((second_half.length)>0)){
         if first_half[0] > second_half[0]{
             output_list.append(second_half.pop(0))
