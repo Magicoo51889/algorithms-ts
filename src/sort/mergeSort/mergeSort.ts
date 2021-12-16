ba89ebdc66aaf6ef100adc7f0a4433c6d8624918
Jack Foot <jackot11f@gmail.com>
2021-12-16 09:55:03 +0000

    Update mergeSort.ts

--- src/sort/mergeSort/mergeSort.ts
+++ src/sort/mergeSort/mergeSort.ts
@@ -1,6 +1,6 @@
 import { SortAlgorithm } from "../common";
 
-type StrOrInt = string | number;
+type StrOrInt = string || number;
 
 const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
     if (items.length === 0){
@@ -14,21 +14,21 @@ const mergeSort: SortAlgorithm<any> = <T>(items: T[]): T[] => {
     if (left_pointer === right_pointer){
         return items[left_pointer]
     }
-    let middle: number = floor((left_pointer + right_pointer) / 2)
+    let middle: number = Math.floor((left_pointer + right_pointer) / 2)
     let first_half: StrOrInt = merge_sort_recurse(items, left_pointer, middle);
     let second_half: StrOrInt = merge_sort_recurse(items, middle + 1, right_pointer);
 
     let output_list: List[StrOrInt] = []
     while(((first_half.length) > 0) && ((second_half.length)>0)){
         if first_half[0] > second_half[0]{
-            output_list.append(second_half.pop(0))
+            output_list.append(second_half.splice[0])
         } else {
-            output_list.append(first_half.pop(0))
+            output_list.append(first_half[0])
         }
     }
 
     output_list.extend(first_half)
     output_list.extend(second_half)
 }
-
+}
 export default mergeSort;
